7f983ca83c42ea08647109f6a1c550b0a0886d60
Nathanael Arnera <Natharnera@live.fr>
2013-10-18 00:10:47 +0200

    add all the sql files

---
+++ ex8.sql
@@ -0,0 +1,15 @@
+delete from pet wher id in ( 
+	select pet.id
+	from pet, person_pet, person
+	where
+	person.id = person_pet.person_ide and
+	pet.id = person_pet.pet_id and
+	person.firts_name = "Zed"
+);
+Select * from pet;
+select * from person_pet;
+delete from person_pet
+	where pet_id not in (
+		select id from pet
+	);
+select * from person_pet;
